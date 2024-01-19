1a4a01dd632ac7c33ab09361239ec67afde3ba34
raymond <raymondezra276@gmail.com>
2024-01-19 20:25:57 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 import searchIcon from './search.svg'
 //  b99969c
 
-const API_URL = 'http://www.omdbapi.com?apikey=b99969c'
+const API_URL = 'https://www.omdbapi.com?apikey=b99969c'
 
 const App = () => {
     const [ movies, setMovies ] = useState([]);
